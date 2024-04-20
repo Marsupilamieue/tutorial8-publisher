8cf51ebfc900460258fec0456e30afcc501b34de
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-04-20 17:56:44 +0700

    Change Username

--- src/main.rs
+++ src/main.rs
@@ -21,23 +21,23 @@ fn main() {
     
     let user1_message = UserCreatedEventMessage {
         user_id: "1".to_owned(),
-        user_name: "129500004y-Amir".to_owned(),
+        user_name: "2206081931y-Amir".to_owned(),
     };
     let user2_message = UserCreatedEventMessage {
         user_id: "2".to_owned(),
-        user_name: "129500004y-Budi".to_owned(),
+        user_name: "2206081931y-Budi".to_owned(),
     };
     let user3_message = UserCreatedEventMessage {
         user_id: "3".to_owned(),
-        user_name: "129500004y-Cica".to_owned(),
+        user_name: "2206081931y-Cica".to_owned(),
     };
     let user4_message = UserCreatedEventMessage {
         user_id: "4".to_owned(),
-        user_name: "129500004y-Dira".to_owned(),
+        user_name: "2206081931y-Dira".to_owned(),
     };
     let user5_message = UserCreatedEventMessage {
         user_id: "5".to_owned(),
-        user_name: "129500004y-Emir".to_owned(),
+        user_name: "2206081931y-Emir".to_owned(),
     };
 
     let _ = publisher.publish_event("user_created".to_owned(), user1_message);
